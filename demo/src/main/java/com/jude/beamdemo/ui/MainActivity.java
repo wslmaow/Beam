c80071ab8f9cd42564446bbd1c8ab65a67242863
langmaowen <wslmaow@163.com>
2018-01-05 15:55:29 +0800

    MotionEvent

--- demo/src/main/java/com/jude/beamdemo/ui/MainActivity.java
+++ demo/src/main/java/com/jude/beamdemo/ui/MainActivity.java
@@ -1,111 +1,105 @@
 package com.jude.beamdemo.ui;
 
-import android.graphics.Color;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.view.Gravity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
+import android.view.MotionEvent;
+import android.view.animation.AnimationSet;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.jude.beam.bijection.RequiresPresenter;
 import com.jude.beam.expansion.BeamBaseActivity;
-import com.jude.beamdemo.adapter.MyPagerAdapter;
 import com.jude.beamdemo.presenter.MainPresenter;
+import com.jude.beamdemo.view.BubblePopupWindow;
 import com.jude.beamdome.R;
 
-import java.util.ArrayList;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import me.relex.circleindicator.CircleIndicator;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Mr.Jude on 2016/2/22.
  */
 @RequiresPresenter(MainPresenter.class)
 public class MainActivity extends BeamBaseActivity<MainPresenter> {
-    ArrayList<String> data=new ArrayList();
+    @Bind(R.id.tv_hello)
     TextView tv_hello;
-    ViewPager viewPager;
-    CircleIndicator indicator;
-    TabLayout tabLayout;
+    BubblePopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_hello=(TextView) findViewById(R.id.tv_hello);
-        viewPager=(ViewPager) findViewById(R.id.viewpager);
-        indicator=(CircleIndicator) findViewById(R.id.indicator);
-        tabLayout=(TabLayout) findViewById(R.id.tablayout);
-        initViewpager();
-        data.add("aaa");
-        data.add("bbb");
-        data.add("ccc");
-        Observable.fromIterable(data).subscribe(new Observer<String>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
+        ButterKnife.bind(this);
 
-            }
+        popupWindow = new BubblePopupWindow(this);
+    }
 
-            @Override
-            public void onNext(@NonNull String s) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                getViewLocation();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                popupWindow.dismiss();
+                float x=event.getX();
+                float y=event.getY();
 
-            }
+                /*AnimatorSet set=new AnimatorSet();
+                ObjectAnimator animator=ObjectAnimator.ofFloat(tv_hello,"translationX",
+                        tv_hello.getX(),x);
+                ObjectAnimator animator2=ObjectAnimator.ofFloat(tv_hello,"translationY"
+                        ,tv_hello.getY(),y);
+                set.setDuration(100).playTogether(animator,animator2);
+                set.start();
+                set.addListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animator) {
 
-            @Override
-            public void onError(@NonNull Throwable e) {
+                    }
 
-            }
+                    @Override
+                    public void onAnimationEnd(Animator animator) {
+                        popupWindow.showAt(tv_hello, Gravity.BOTTOM);
+                    }
 
-            @Override
-            public void onComplete() {
+                    @Override
+                    public void onAnimationCancel(Animator animator) {
 
-            }
-        });
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animator) {
+
+                    }
+                });*/
+                return true;
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
-        if(hasFocus){
-            com.jude.beamdemo.view.BubblePopupWindow popupWindow=new com.jude.beamdemo.view.BubblePopupWindow(this);
-            popupWindow.showAt(tv_hello,Gravity.BOTTOM);
+        if (hasFocus) {
+            getViewLocation();
+            BubblePopupWindow popupWindow = new BubblePopupWindow(this);
+//            popupWindow.showAt(tv_hello, Gravity.BOTTOM);
         }
     }
-    private void initViewpager(){
-        ImageView view=new ImageView(this);
-        ViewGroup.LayoutParams params=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        view.setLayoutParams(params);
-        view.setBackgroundColor(Color.parseColor("#440093ff"));
-
-        ArrayList<View> contemts=new ArrayList<>();
-        contemts.add(view);
-        ImageView view1=new ImageView(this);
-        ViewGroup.LayoutParams params1=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        view1.setLayoutParams(params1);
-        view1.setBackgroundColor(Color.parseColor("#ee9cff"));
-        contemts.add(view1);
-        ImageView view2=new ImageView(this);
-        ViewGroup.LayoutParams params2=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        view2.setLayoutParams(params2);
-        view2.setBackgroundColor(Color.parseColor("#339c55"));
-        contemts.add(view2);
-
-        viewPager.setAdapter(new MyPagerAdapter(contemts));
-        indicator.setViewPager(viewPager);
-        tabLayout.setupWithViewPager(viewPager);
-        tabLayout.setTabMode(TabLayout.MODE_FIXED);
-        TextView textView=new TextView(this);
-        FrameLayout.LayoutParams layoutParams=new FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        textView.setText("Tab1");
-        textView.setLayoutParams(layoutParams);
-        tabLayout.getTabAt(0).setCustomView(textView);
+    private void getViewLocation(){
+        int left = tv_hello.getLeft();
+        int top = tv_hello.getTop();
+        int right = tv_hello.getRight();
+        int bottom = tv_hello.getBottom();
+        float x = tv_hello.getX();
+        float y = tv_hello.getY();
+        float translationX = tv_hello.getTranslationX();
+        float translationY = tv_hello.getTranslationY();
+        float scrollX=tv_hello.getScrollX();
+        float scrollY=tv_hello.getScrollY();
     }
 }
