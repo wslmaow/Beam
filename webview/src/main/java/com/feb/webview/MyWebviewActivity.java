ea7065537864f7426bb6d0c23ddfc7c2c0d13628
langmaowen <wslmaow@163.com>
2018-02-27 10:13:29 +0800

    webView

---
+++ webview/src/main/java/com/feb/webview/MyWebviewActivity.java
@@ -0,0 +1,103 @@
+package com.feb.webview;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MyWebviewActivity extends AppCompatActivity {
+
+    @butterknife.Bind(R.id.title)
+    TextView title;
+    @butterknife.Bind(R.id.webView)
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_webview);
+        butterknife.ButterKnife.bind(this);
+        initParams();
+        initView();
+        //initListener();
+    }
+
+    private void initParams(){}
+
+    private void initView(){
+        //webView.loadUrl("http://fanyi.baidu.com/?aldtype=16047#en/zh/NARROW");
+        //webView.loadUrl("http://m.hexun.com/insurance/2017-12-11/191937627.html?from=timeline&isappinstalled=0%22");
+        webView.loadUrl("http://www.mobius-china.com/taibaovideo//");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.startsWith("http://") || url.startsWith("https://")) {
+                    view.loadUrl(url);
+
+                } else {
+                    Intent intent = new Intent();
+                    intent.setAction(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    try {
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+        });
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+       /* webSettings.setAllowContentAccess(true);
+        webSettings.setPluginState(WebSettings.PluginState.ON);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setBlockNetworkImage(false);//解决图片不显示
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.LOLLIPOP){
+            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }*/
+
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(true);
+
+    }
+
+    private void initListener(){
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                if(keyEvent.getAction()==KeyEvent.ACTION_DOWN){
+                    if (i==KeyEvent.KEYCODE_BACK && webView.canGoBack()){
+                        webView.goBack();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode==KeyEvent.ACTION_DOWN){}
+        return super.onKeyDown(keyCode, event);
+    }
+}
